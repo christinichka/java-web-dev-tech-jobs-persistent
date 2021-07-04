3a6b34c5e7c6478db2bfd82bab8518a31eb59107
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-07-04 13:22:54 -0500

    Add files via upload
    Committing files

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -0,0 +1,105 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "My Jobs");
+
+        List jobs = (List<Job>) jobRepository.findAll();
+        model.addAttribute("jobs", jobs );
+
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String displayAddJobForm(Model model) {
+        model.addAttribute("title", "Add Job");
+        model.addAttribute(new Job());
+
+        List employers = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employers);
+
+        List skills = (List<Skill>) skillRepository.findAll();
+        model.addAttribute("skills", skills);
+
+        return "add";
+    }
+
+    @PostMapping("add")
+    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
+                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Job");
+            List employers = (List<Employer>) employerRepository.findAll();
+            model.addAttribute("employers", employers);
+
+
+            return "add";
+        }
+
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+            if (optEmployer.isPresent()) {
+                Employer employer = optEmployer.get();
+                newJob.setEmployer(employer);
+
+            }
+
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
+
+        jobRepository.save(newJob);
+
+
+        return "redirect:./";
+        }
+
+
+    @GetMapping("view/{jobId}")
+    public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional<Job> optJob = jobRepository.findById(jobId);
+        if(optJob.isPresent()) {
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        }
+        else {
+            return "redirect:./";
+        }
+
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -0,0 +1,42 @@
+package org.launchcode.techjobs.persistent.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Job extends AbstractEntity {
+
+    @ManyToOne
+    private Employer employer;
+
+    @ManyToMany
+    @NotNull(message = "Must choose a skill to continue.")
+    private List<Skill> skills;
+
+    public Job() {
+    }
+
+    public Job(Employer anEmployer, List<Skill> someSkills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = someSkills;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
